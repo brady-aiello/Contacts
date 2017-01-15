de18c78f86a5cfccb01bde2e45a27f6e88017711
Brady <brady.aiello@gmail.com>
2017-01-14 18:28:29 -0800

    Initial commit
    Signed-off-by: Brady <brady.aiello@gmail.com>

---
+++ public/controllers/controller.js
@@ -0,0 +1,34 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('AppCtrl', ['$scope', '$http',	function($scope, $http) {
+	
+	var refresh = function(){
+		$http.get('/contactlist').success(function(response){
+			console.log("I got the data I requested");
+			$scope.contactlist = response;
+		});
+	};
+	refresh();
+
+	$scope.addContact = function(){
+		console.log($scope.contact);
+		$http.post('/contactlist', $scope.contact).success(function(response){
+			console.log(response);
+			refresh();
+		});
+	};	
+
+	$scope.remove = function(id){
+		console.log(id);
+		$http.delete('/contactlist/' + id).success(function(response){
+			refresh();
+		});
+	};
+
+	$scope.edit = function(id){
+		$http.get('/contactlist/' + id).success(function(response){
+			$scope.contact = response;
+		});
+	}
+
+}]);
